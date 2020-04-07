f285009d1e0554596f2812d1e82629ffb3363302
Naomoreira <naomi.o.moreira@gmail.com>
2020-04-07 13:04:28 -0400

    bubblesort function

---
+++ bubblesort.js
@@ -0,0 +1,35 @@
+const tootsiepop = {};
+
+tootsiepop.lick = function () {
+  return "licked";
+};
+tootsiepop.getToCenter = function () {
+  this.lick();
+  this.lick();
+  this.lick();
+  return "got to center";
+};
+
+function swap(num, num2) {
+//   let number = array[j];
+//   array[j] = array[j + 1];
+//   array[j + 1] = number;
+let number = num;
+  num = num2;
+  num2 = number;
+}
+
+function bubbleSort(array) {
+  const length = array.length;
+  const count = 0;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      if (array[j] < array[j + 1]) {
+        swap(array[j], array[j + 1]); 
+        count++;
+      }
+    }
+  }
+  console.log(count);
+  return array;
+}
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,6 +1,14 @@
-import testem
+// import testem
 
 describe('Bubble Sort', function(){
+  beforeAll(function () {
+    spyOn(tootsiepop, 'lick').and.callThrough(); // replace existing `tootsiepop['lick']` method
+  });
+  it('getting to the center of tootsiepop involves exactly three licks', function () {
+    tootsiepop.getToCenter();
+    expect(tootsiepop.lick.calls.count()).toEqual(3);
+  });
+  
   it('handles an empty array', function(){
     expect( bubbleSort([]) ).toEqual( [] );
   });
